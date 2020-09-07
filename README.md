22ae76fbc5de1223bd197d0693b064da7d150f93
Pete Savage <psavage@redhat.com>
2020-09-07 08:33:57 +0100

    Simple Test now works

---
+++ README.md
@@ -0,0 +1,9 @@
+app-common-go
+=============
+
+Simple client access library for the config for the Whippoorwill operator.
+
+Testing
+-------
+
+`ACG_CONFIG="../../../test.json" go test -v ./pkg/api/v1/...`
--- pkg/api/v1/config_test.go
+++ pkg/api/v1/config_test.go
@@ -1,9 +1,12 @@
 package v1
 
 import (
+	"log"
 	"testing"
 )
 
 func TestClientLoad(t *testing.T) {
-	loadConfig("test.json")
+	if LoadedConfig.Kafka.Brokers[0].Port != 27015 {
+		log.Fatal("Kafka port was not loaded")
+	}
 }
