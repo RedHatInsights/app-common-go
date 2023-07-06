f66696256b1230f84f849aee2046b88dbe2f9ba9
Adam Drew <addrew@redhat.com>
2023-07-06 13:25:14 -0400

    naming

--- sync_config.sh
+++ sync_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # function to check if gojsonschema command is installed and if not install it
-function check_gojsonschema {
+function check_and_install_gojsonschema {
     if ! command -v gojsonschema &> /dev/null
     then
         echo "gojsonschema could not be found"
@@ -10,7 +10,7 @@ function check_gojsonschema {
     fi
 }
 
-check_gojsonschema
+check_and_install_gojsonschema
 cd pkg/api/v1
 wget https://raw.githubusercontent.com/RedHatInsights/clowder/master/controllers/cloud.redhat.com/config/schema.json -O schema.json
 gojsonschema -p v1 -o types.go schema.json
