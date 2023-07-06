d2469bc6eeff652ef5119cfed18f7466910ebdd8
Adam Drew <addrew@redhat.com>
2023-07-06 13:24:31 -0400

    Sync config and update sync_config script to install gosjonschema

--- sync_config.sh
+++ sync_config.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# function to check if gojsonschema command is installed and if not install it
+function check_gojsonschema {
+    if ! command -v gojsonschema &> /dev/null
+    then
+        echo "gojsonschema could not be found"
+        echo "installing gojsonschema"
+        go install github.com/atombender/go-jsonschema/cmd/gojsonschema@latest
+    fi
+}
+
+check_gojsonschema
 cd pkg/api/v1
 wget https://raw.githubusercontent.com/RedHatInsights/clowder/master/controllers/cloud.redhat.com/config/schema.json -O schema.json
 gojsonschema -p v1 -o types.go schema.json
